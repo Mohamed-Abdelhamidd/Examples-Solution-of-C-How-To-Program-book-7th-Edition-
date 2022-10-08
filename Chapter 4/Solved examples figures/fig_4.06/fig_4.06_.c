b693a9877bd866a610718205375948d8bd968d72
Mohamed-Abdelhamidd <mohamedd.abdellhamidd@gmail.com>
2022-10-08 15:56:21 +0200

    added fig 4.06

--- Chapter 4/Solved examples figures/fig_4.06/fig_4.06_.c
+++ Chapter 4/Solved examples figures/fig_4.06/fig_4.06_.c
@@ -1,25 +1,20 @@
-
+//fig 4.6: calculating compound interest using for statement
 
 #include <stdio.h>
+#include <math.h>  //to include math functions
 
 int main (void)
 {
-	double p;
-	double r;
-	double a;
-	unsigned int n;
-	unsigned int c;
+	float principal = 1000;
+	float rate = 0.05;
+	unsigned int year;
+	float amount;
 
-	printf ("Enter years: ");
-	scanf ("%u", &n);
-	printf ("enter interest rate: ");
-	scanf ("%f", &r);
-	printf ("Enter the invested money: ");
-	scanf ("%f", &p);
+	printf ("%4s%21s", "year", "Amount on deposit\n"); //%4s to add 4 spaces for the string year, and %21 add 21 spaces for string amount on deposit and put it at most left
 
-	for (c = 1; c <= n; ++c){
-
+	for (year = 1; year <= 10; ++year){		//initial the year with 1, and show each year calculation with the new year so we used it at the for statement
+		amount = principal * pow (1 + rate, year);	// pow (the number, the power)
+		printf ("%4u%20.2f\n", year, amount); //%20.2f : 20 is adding space for number with 20 space, .2 showing the float numbers after point
 	}
+	return 0;
 }
-
-
